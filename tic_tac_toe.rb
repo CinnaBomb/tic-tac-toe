4067d68aa3ab9864da683ba5019335d401b65dae
Laura <Laura.Stephens@utexas.edu>
2017-01-21 01:13:12 -0600

    finished and debugged starter methods

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,58 +1,83 @@
 class Game
 
-	def initialize()
-		draw_new_board
-	end
-
-	def draw_new_board
-		array = [1,2,3,4,5,6,7,8,9]
-		update_board(array)
+	def initialize
 		@player_symbol = "X"
 	end
 
-	def update_board (a)
-
-		puts "_____________"
-		puts "| #{a[0]} | #{a[1]} | #{a[2]} |"
-		puts "_____________"
-		puts "| #{a[3]} | #{a[4]} | #{a[5]} |"
-		puts "_____________"
-		puts "| #{a[6]} | #{a[7]} | #{a[8]} |"
-		puts "_____________"
-
-		puts "[#{a[0]}][#{a[1]}][#{a[2]}]"
-		puts "[#{a[3]}][#{a[4]}][#{a[5]}]"
-		puts "[#{a[6]}][#{a[7]}][#{a[8]}]"
-
+	def new_board
+		@a = [1,2,3,4,5,6,7,8,9]
 	end
 
-	def turn(player_symbol)
+	def update_board 
+		puts "_____________"
+		puts "| #{@a[0]} | #{@a[1]} | #{@a[2]} |"
+		puts "_____________"
+		puts "| #{@a[3]} | #{@a[4]} | #{@a[5]} |"
+		puts "_____________"
+		puts "| #{@a[6]} | #{@a[7]} | #{@a[8]} |"
+		puts "_____________"
 
-		square = gets.chomp.downcase("Type the number of a square on the board!")
-
-
+		#Alternate View
+		#puts "[#{@a[0]}][#{@a[1]}][#{@a[2]}]"
+		#puts "[#{@a[3]}][#{@a[4]}][#{@a[5]}]"
+		#puts "[#{@a[6]}][#{@a[7]}][#{@a[8]}]"
 	end
 
-	def o_turn(square)
-		puts "O's turn"
-
+	def turn
+		puts "#{@player_symbol}'s Turn!"
+		loop do
+			puts "Type the number of a square on the board!"
+			@square = gets.downcase.chomp
+			break if @square == "q" || valid_move
+		end
+		if @square == "q"
+			puts "Thanks for playing!"
+			exit
+		end
+		i = @square.to_i-1
+		@a[i] = @player_symbol
+		update_board
 	end
 
-	def game_won
-
+	def switch
+		if @player_symbol == "X"
+			@player_symbol = "O"
+		else
+			@player_symbol = "X"
+		end
 	end
 
-	def valid_move (square)
+	def game_won?
+		winning_array = [
+			[1, 2, 3],
+			[4, 5, 6],
+			[7, 8, 9],
+			[1, 4, 7],
+			[2, 5, 8],
+			[3, 6, 9],
+			[1, 5, 9],
+			[3, 5, 7] ]
+			puts winning_array.any?{|set| @a.include?(set)}.inspect
+		winning_array.any?{|set| @a.include?(set)}
+	end
 
+	def game_over
+		puts "#{@player_symbol} won!"
+	end
+
+	def valid_move 
+		@square = @square.to_i
+		return true if @a.include?(@square)
 	end
 
 	def play
 		puts "Let's play tic-tac-toe!"
 		puts "The choices are square 1-9"
-		draw_new_board
+		new_board
+		update_board
 		until game_won?
-		puts "#{player_symbol}'s Turn!"
-		turn
+			turn
+			switch
 		end
 	end
 
